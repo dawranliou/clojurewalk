827b2e26a793ade120a41f7696978a060ea7bce5
Daw-Ran Liou <dawran6@gmail.com>
2019-08-25 09:40:51 -0700

    Embed nrepl server

--- src/server.clj
+++ src/server.clj
@@ -1,11 +1,14 @@
 (ns server
   (:require [coast]
-            [routes])
+            [routes]
+            [nrepl.server :as nrepl])
   (:gen-class))
 
 (def app (coast/app {:routes routes/routes}))
 
 (defn -main [& [port]]
+  (println "nrepl starting on port 7888")
+  (defonce server (nrepl/start-server :port 7888))
   (coast/server app {:port port}))
 
 (comment
