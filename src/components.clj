faf6360a65ecd53a9881d3d0234426bef010373d
Daw-Ran Liou <dawran6@gmail.com>
2019-08-30 15:40:21 -0700

    Add doctype

--- src/components.clj
+++ src/components.clj
@@ -1,28 +1,31 @@
 (ns components
-  (:require [coast]))
+  (:require [coast]
+            [hiccup.page :refer [doctype]]))
 
 (defn layout [request body]
-  [:html
-   [:head
-    [:title "Clojure/Walk"]
-    [:meta {:name "description" :content "Clojure tutorials and resources."}]
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
-    [:meta {:name "twitter:card" :content "summary"}]
-    [:meta {:name "twitter:creator" :content "@dawranliou"}]
-    [:meta {:property "og:url" :content "https://clojurewalk.com"}]
-    [:meta {:property "og:title" :content "Clojure/Walk"}]
-    [:meta {:property "og:description" :content "Clojure tutorials and resources."}]
-    [:meta {:property "og:image" :content "https://clojurewalk.com/assets/img/ClojureWalkLogoSquareBlack.png"}]
-    [:meta {:property "og:image:type" :content "image/png"}]
-    [:meta {:property "og:image:width" :content "720"}]
-    [:meta {:property "og:image:height" :content "720"}]
-    [:link {:rel= "short icon" :type "image/x-icon" :href "/favicon.ico"}]
-    [:link {:rel= "icon" :type "image/x-icon" :href "/favicon.ico"}]
-    [:link {:href "https://fonts.googleapis.com/css?family=IBM+Plex+Mono:300&display=swap" :rel "stylesheet"}]
-    (coast/css "bundle.css")
-    (coast/js "bundle.js")
-    [:body.bg-black-cw
-     body]]])
+  (list
+    (doctype :html5)
+    [:html
+     [:head
+      [:title "Clojure/Walk"]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+      [:meta {:name "description" :content "Clojure tutorials and resources."}]
+      [:meta {:name "twitter:card" :content "summary"}]
+      [:meta {:name "twitter:creator" :content "@dawranliou"}]
+      [:meta {:property "og:url" :content "https://clojurewalk.com"}]
+      [:meta {:property "og:title" :content "Clojure/Walk"}]
+      [:meta {:property "og:description" :content "Clojure tutorials and resources."}]
+      [:meta {:property "og:image" :content "https://clojurewalk.com/assets/img/ClojureWalkLogoSquareBlack.png"}]
+      [:meta {:property "og:image:type" :content "image/png"}]
+      [:meta {:property "og:image:width" :content "720"}]
+      [:meta {:property "og:image:height" :content "720"}]
+      [:link {:rel= "short icon" :type "image/x-icon" :href "/favicon.ico"}]
+      [:link {:rel= "icon" :type "image/x-icon" :href "/favicon.ico"}]
+      [:link {:href "https://fonts.googleapis.com/css?family=IBM+Plex+Mono:300&display=swap" :rel "stylesheet"}]
+      (coast/css "bundle.css")
+      (coast/js "bundle.js")
+      [:body.bg-black-cw
+       body]]]))
 
 (defn link-to [url & body]
   [:a {:href url :class "f6 link underline blue"}
