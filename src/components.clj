82a5fe3b15b044538ea51dbc905ed7ef1e6be4b8
Daw-Ran Liou <dawran6@gmail.com>
2019-08-30 11:12:45 -0700

    Add icon

--- src/components.clj
+++ src/components.clj
@@ -5,6 +5,8 @@
   [:html
    [:head
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
+    [:link {:rel= "short icon" :type "image/x-icon" :href "/favicon.ico"}]
+    [:link {:rel= "icon" :type "image/x-icon" :href "/favicon.ico"}]
     (coast/css "bundle.css")
     (coast/js "bundle.js")]
    [:body
