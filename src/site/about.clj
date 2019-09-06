7b7b275ce8daded5b6c58fc4dea0b30c098ae7dc
Daw-Ran Liou <dawran6@gmail.com>
2019-09-06 10:14:40 -0700

    Draft about page

--- src/site/about.clj
+++ src/site/about.clj
@@ -1,7 +1,16 @@
-(ns site.about)
+(ns site.about
+  (:require [components :refer [container]]))
 
 (defn index
   [request]
-  [:div.tc
-   [:p.f1.dib.mono.bg-green-cw
-    "Coming soon!"]])
+  (container
+    {:mw 6}
+    [:article.measure
+     [:h1.tc "About "
+      [:span.mono.bg-green-cw.ttu.fw4 "Clojure/Walk"]]
+     [:p.lh-copy
+      "We want to help beginner-to-intermediate fellow Clojurists to improve your skills!
+Clojure is powerful. However, in order to be effective in Clojure there are many alient
+things most of us need to figure out ourself, especially when there's no one there to 'show'
+you. Clojure/Walk is a place to learn Clojure and Clojure/Script libraries. We want to curate
+video contents and articles for beginners."]]))
