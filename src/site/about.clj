a1091caf4b8b2c73cf02fd41f4ca25187162933b
Daw-Ran Liou <dawran6@gmail.com>
2019-09-09 10:16:15 -0700

    Update about

--- src/site/about.clj
+++ src/site/about.clj
@@ -1,6 +1,20 @@
 (ns site.about
   (:require [components :refer [container]]))
 
+(def about-content
+  ["I want to help you, fellow beginner Clojurists, to know your craft.
+When I started learning Clojure, I was desperate to find someone to
+show me what I can do with the language.
+Clojure is powerful. However, to be effective there are many alien
+territories any Clojurist need to force themselves into
+(especially when we came to Clojure with the presumptions from our previous programming experiences.)"
+   "Clojure/Walk is a place to learn Clojure and ClojureScript libraries.
+By showing you the libraries, you will learn about: REPL driven development,
+the Clojure ecosystem, and how to be on your own."
+   "I hope you enjoy the contents. At this point, this is my side project
+and I will post updates as frequently as I could. If you have any suggestion,
+you're welcome to contact me"])
+
 (defn index
   [request]
   (container
@@ -8,9 +22,11 @@
     [:article.measure
      [:h1.tc "About "
       [:span.mono.bg-green-cw.ttu.fw4 "Clojure/Walk"]]
-     [:p.lh-copy
-      "We want to help beginner-to-intermediate fellow Clojurists to improve your skills!
-Clojure is powerful. However, in order to be effective in Clojure there are many alient
-things most of us need to figure out ourself, especially when there's no one there to 'show'
-you. Clojure/Walk is a place to learn Clojure and Clojure/Script libraries. We want to curate
-video contents and articles for beginners."]]))
+     (for [paragraph about-content]
+       [:p.lh-copy paragraph])
+     [:p.tr
+      [:a {:href "mailto://dawran6@gmail.com"} "dawran6@gmail.com"]]
+     [:p.tr
+      [:a {:href "https://twitter.com/dawranliou"} "@dawranliou"]]
+     [:p.tr
+      "~ Daw-Ran Liou 2019/9/19"]]))
