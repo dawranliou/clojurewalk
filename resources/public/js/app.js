0646f67a1abb0c559c39f84811ad1c851e09800a
Daw-Ran Liou <dawran6@gmail.com>
2019-09-10 14:11:09 -0700

    Show post preview

--- resources/public/js/app.js
+++ resources/public/js/app.js
@@ -13,6 +13,47 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+    // show editor
+    document.body.addEventListener("click", function(e) {
+        if (e.target.id === "edit") {
+            document.getElementById("form-container").classList.remove("dn");
+            document.getElementById("preview-container").classList.add("dn");
+            e.target.classList.add("blue");
+            e.target.classList.remove("gray");
+            document.getElementById("preview").classList.remove("blue");
+            document.getElementById("preview").classList.add("gray");
+        }
+    });
+
+    // show preview
+    document.body.addEventListener("click", function(e) {
+        if (e.target.id === "preview") {
+            e.preventDefault();
+            e.target.classList.add("blue");
+            e.target.classList.remove("gray");
+            document.getElementById("form-container").classList.add("dn");
+            document.getElementById("edit").classList.remove("blue");
+            document.getElementById("edit").classList.add("gray");
+
+            data = new FormData();
+            data.append("title",document.getElementsByName("post/title")[0].value);
+            data.append("body", document.getElementsByName("post/body")[0].value);
+            data.append("__anti-forgery-token", document.getElementsByName("__anti-forgery-token")[0].value);
+
+            fetch("/posts/preview", {
+                method: "POST",
+                body: data,
+                credentials: 'same-origin'
+            })
+                .then(response => response.text())
+                .then(text => {
+                    var preview = document.getElementById("preview-container");
+                    preview.innerHTML = text;
+                    preview.classList.remove("dn");
+                });
+        }
+    });
+
     // auto save
     var timer = null;
     var saveCount = 0;
@@ -39,7 +80,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
                 request.onload = function(event) {
                     timer = null;
-                    console.log(request);
                     if (request.status == 200) {
                         status = "Saved";
                         var response = JSON.parse(request.response);
