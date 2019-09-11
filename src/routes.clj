dc65fc5b4e2dd6b7f7d9357f7015e5cdcb85250e
Daw-Ran Liou <dawran6@gmail.com>
2019-09-11 09:15:12 -0700

    Style video page

--- src/routes.clj
+++ src/routes.clj
@@ -7,40 +7,71 @@
 (def routes
   (coast/routes
 
-    (coast/site
-      (coast/with-layout layouts/doc-layout
-        [:get "/read/:post-slug" :site.article/reader])
+   (coast/site
+    (coast/with-layout layouts/doc-layout
+      [:get "/read/:post-slug" :site.article/reader])
 
-      (coast/with-layout layouts/layout
-        ;; home
-        [:get "/" :site.home/index]
+    (coast/with-layout layouts/layout
+      ;; home
+      [:get "/" :site.home/index]
 
-        ;; videos
-        [:get "/watch" :site.video/index]
-        [:get "/watch/:youtubeid" :site.video/player]
+      ;; videos
+      [:get "/watch" :site.video/index]
+      [:get "/watch/:youtubeid" :site.video/player]
 
-        ;; articles
-        [:get "/read" :site.article/index]
+      ;; articles
+      [:get "/read" :site.article/index]
 
-        ;; about
-        [:get "/about" :site.about/index]
+      ;; about
+      [:get "/about" :site.about/index]
 
-        ;; subscribe-to-newsletter
-        [:post "/subscribe-to-newsletter" :site.about/subscribe]
+      ;; subscribe-to-newsletter
+      [:post "/subscribe-to-newsletter" :site.about/subscribe]
 
-        ;; admin
-        [:get "/admin/sign-in" :admin/sign-in]
-        [:post "/admin/sign-in" :admin/create-session]
-        (coast/with
-          middleware/auth middleware/current-member
-          [:resource :video]
-          [:resource :maillist]
-          [:resource :series]
-          [:resource :post]
-          [:post "/posts/preview" :post/preview]
-          [:get "/admin" :admin/dashboard]
-          [:post "/admin/sign-out" :admin/delete-session])))
+      ;; admin
+      [:get "/admin/sign-in" :admin/sign-in]
+      [:post "/admin/sign-in" :admin/create-session]
 
-    (coast/api
-      (coast/with-prefix "/api"
-        [:get "/" :api.home/index]))))
+      (coast/with
+       middleware/auth middleware/current-member
+
+       [:get "/admin" :admin/dashboard]
+       [:post "/admin/sign-out" :admin/delete-session]
+
+       [:get    "/videos"          :video/index]
+       [:get    "/videos/:id"      :video/view]
+       [:get    "/videos/build"    :video/build]
+       [:get    "/videos/:id/edit" :video/edit]
+       [:post   "/videos"          :video/create]
+       [:put    "/videos/:id"      :video/change]
+       [:delete "/videos/:id"      :video/delete]
+
+       [:get    "/maillists"          :maillist/index]
+       [:get    "/maillists/:id"      :maillist/view]
+       [:get    "/maillists/build"    :maillist/build]
+       [:get    "/maillists/:id/edit" :maillist/edit]
+       [:post   "/maillists"          :maillist/create]
+       [:put    "/maillists/:id"      :maillist/change]
+       [:delete "/maillists/:id"      :maillist/delete]
+
+       [:get    "/seriess"          :series/index]
+       [:get    "/seriess/:id"      :series/view]
+       [:get    "/seriess/build"    :series/build]
+       [:get    "/seriess/:id/edit" :series/edit]
+       [:post   "/seriess"          :series/create]
+       [:put    "/seriess/:id"      :series/change]
+       [:delete "/seriess/:id"      :series/delete]
+
+       [:get    "/posts"          :post/index]
+       [:get    "/posts/:id"      :post/view]
+       [:get    "/posts/build"    :post/build]
+       [:get    "/posts/:id/edit" :post/edit]
+       [:post   "/posts"          :post/create]
+       [:put    "/posts/:id"      :post/change]
+       [:delete "/posts/:id"      :post/delete]
+
+       [:post "/posts/preview" :post/preview])))
+
+   (coast/api
+    (coast/with-prefix "/api"
+      [:get "/" :api.home/index]))))
--- src/site/video.clj
+++ src/site/video.clj
@@ -2,30 +2,39 @@
   (:require [components :refer [container]]
             [coast]))
 
+(defn card
+  [title link {:keys [background-image] :as opts}]
+  [:div.fl.w-100.h5.w-30-l.cover.bg-center.dt.ba.br2.mb3
+   (when background-image {:class "hide-child" :style background-image})
+   [:a.link.dtc.v-mid.w-100.h-100.pa5.tc
+    (if background-image
+      {:href  link
+       :class "child bg-black-90 white"}
+      {:href  link
+       :class "black dim"})
+    title]])
+
 (defn index
   [request]
   (let [serieses (coast/q '[:pull series/title {:series/videos [video/title video/youtubeid]} :from series])]
     (container
-      {:mw 8}
-      (for [{:series/keys [title videos]} serieses]
-        [:div.cf.mv3
-         [:h2 title]
-         (for [{:video/keys [title youtubeid]} videos
-               :let                            [link (str "https://i.ytimg.com/vi/" youtubeid "/sddefault.jpg")]]
-           [:div.fl.w-100.w-third-ns.pa2.lift
-            [:p.black-cw
-             title]
-            [:a.link.dim
-             {:href (coast/url-for :site.video/player {:youtubeid youtubeid})}
-             [:img {:alt (str "The thumbnail of " title)
-                    :src link}]]])]))))
+     {:mw 8}
+     (for [{:series/keys [title videos]} serieses]
+       [:div.cf.mv3
+        [:h2.f3.mono.bg-green-cw.pa4 (str "> " title)]
+        [:div.flex.flex-column.flex-row-l.justify-between
+         (for [{:video/keys [title youtubeid]} (take 2 videos)]
+           (card title
+                 (coast/url-for :site.video/player {:youtubeid youtubeid})
+                 {:background-image (str "background-image: url(https://i.ytimg.com/vi/" youtubeid "/sddefault.jpg)")}))
+         (card "Watch series >>" "#" {})]]))))
 
 (defn player
   [request]
   (let [youtubeid   (-> request :params :youtubeid)
         row         (coast/pluck
-                      '[:select * :from video :where [youtubeid ?youtubeid]]
-                      {:youtubeid youtubeid})
+                     '[:select * :from video :where [youtubeid ?youtubeid]]
+                     {:youtubeid youtubeid})
         title       (:video/title row)
         description (:video/description row)]
     [:div.pa4.w-100.center.mw8
@@ -42,12 +51,12 @@
 (comment
   (def youtubeid "bPz4-Vcx27A")
   (coast/pluck
-    '[:select * :from video :where [youtubeid ?youtubeid]]
-    {:youtubeid youtubeid})
+   '[:select * :from video :where [youtubeid ?youtubeid]]
+   {:youtubeid youtubeid})
   (coast/q
-    '[:select video/youtubeid video/title series/name
-      :from video :join [series video/series series/id]])
+   '[:select video/youtubeid video/title series/name
+     :from video :join [series video/series series/id]])
   (coast/q
-    '[:pull *
-      :from series])
+   '[:pull *
+     :from series])
   (coast/q '[:pull series/name {:series/videos [video/title video/youtubeid]} :from series]))
